584f7c3d646bee850991bb76151eaa054ed3dea6
Sergio Peralta <seperalta88@gmail.com>
2022-09-26 11:30:04 -0300

    first scaffolding and project setup

---
+++ client/src/app/pages/login/login.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './components/login-page/login.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+
+@NgModule({
+  declarations: [LoginComponent, LoginFormComponent],
+  imports: [CommonModule, LoginRoutingModule],
+})
+export class LoginModule {}
