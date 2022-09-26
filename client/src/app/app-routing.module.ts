be3825154c64e085903ea036daf423a0a1e7493b
Sergio Peralta <seperalta88@gmail.com>
2022-09-26 16:52:31 -0300

    added json-server to emulate the backend

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,14 +3,33 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'login',
+    path: 'auth',
     loadChildren: () =>
-      import('./pages/login/login.module').then((m) => m.LoginModule),
+      import('./pages/auth/auth.module').then((m) => m.AuthModule),
   },
   {
     path: 'home',
     loadChildren: () =>
       import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./pages/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./pages/not-found/not-found.module').then(
+        (m) => m.NotFoundModule
+      ),
   },
 ];
 
---
+++ client/src/app/pages/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './pages/login-page/login.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { SingupPageComponent } from './pages/singup-page/singup-page.component';
+import { MaterialModule } from '../../material/material.module';
+import { SingupFormComponent } from './components/singup-form/singup-form.component';
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    LoginFormComponent,
+    SingupPageComponent,
+    SingupFormComponent,
+  ],
+  imports: [CommonModule, LoginRoutingModule, MaterialModule],
+})
+export class AuthModule {}
