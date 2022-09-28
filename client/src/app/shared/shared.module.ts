dd62cc02fcfe30ec96b942716250f768a102a47c
Willy Bardales <willybc06@gmail.com>
2022-09-28 11:45:43 -0300

    component header first step

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 
+import { MaterialModule } from '../material/material.module';
+import { AppRoutingModule } from '../app-routing.module';
+
 @NgModule({
   declarations: [FooterComponent, HeaderComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    AppRoutingModule],
   exports: [FooterComponent, HeaderComponent],
 })
-export class SharedModule {}
+export class SharedModule { }
