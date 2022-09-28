ac1090f20ecb92a1721c15fd626b547cef9e5dd7
Sergio Peralta <seperalta88@gmail.com>
2022-09-28 11:16:07 -0300

    layout changes

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [FooterComponent, HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MaterialModule],
   exports: [FooterComponent, HeaderComponent],
 })
 export class SharedModule {}
